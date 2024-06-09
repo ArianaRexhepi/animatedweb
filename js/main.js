5c237cebab842ecd1672c3d3827c4397c2fa5800
Ariana Rexhepi <arianaarexhepi@gmail.com>
2024-06-09 16:05:11 +0200

    finished with the front page

--- js/main.js
+++ js/main.js
@@ -1,23 +1,28 @@
-    let stars = document.getElementById('stars');
-    let moon = document.getElementById('moon');
-    let text = document.getElementById('text');
-    let btn = document.getElementById('btn');
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let text = document.getElementById('text');
+var btn = document.querySelector('.btn');
+let header = document.querySelector('header');
 
-    window.addEventListener('scroll', function() {
-      let value = window.scrollY;
-      text.style.marginRight = value * 4 +'px';
-      text.style.marginTop = value * 1.5 +'px';
-      btn.style.marginTop = value * 1.5 +'px';
+window.addEventListener('scroll', function() {
+    let value = window.scrollY;
+    text.style.marginTop = value * 1.5 + 'px';
+    btn.style.marginTop = value * 1 + 'px';
 
-    });
-    
-    
-    let amplitude = 180; 
-    function updateMoonPosition() {
+    let hideThreshold = 520; 
+    if (value > hideThreshold) {
+        moon.style.display = 'none';
+    } else {
+        moon.style.display = 'block'; 
+    }
+});
+
+
+let amplitude = 180; 
+function updateMoonPosition() {
     let textRect = text.getBoundingClientRect();
     let centerX = textRect.left + textRect.width / 2;
     let centerY = textRect.top + textRect.height / 2;
-
 
     let time = performance.now() * 0.001; 
     let xOffset = Math.sin(time) * amplitude;
@@ -28,7 +33,6 @@
 
     moon.style.left = x + 'px';
     moon.style.top = y + 'px';
-    }
-    setInterval(updateMoonPosition, 10); 
+}
 
-    
+setInterval(updateMoonPosition, 10); 
